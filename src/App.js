9d219d5790a36acdc2064ec7700fa24cfcd490c8
Atefeh_MN <atefeh.nasrabadi1993@gmail.com>
2021-12-16 20:42:41 +0330

    improve homePage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import {
 } from "react-router-dom";
 import './App.css';
 import Layout from "./layout/Layout";
+import CartPage from "./pages/CartPage";
 import Home from "./pages/HomePage";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path='/' exact element={<Home/>}/>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/cart' element={<CartPage/>}/>
       </Routes>
     </Layout>
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,28 @@
-
+import * as data from '../data'
 const Home = () => {
-    return (<div>
-        This is home page
-    </div> );
+    const addCartHandler = (product) => {
+        console.log(product)
+    }
+    return (
+        <main className='container'>
+            <section  className='productList'>
+                {data.products.map((product) =>{return (
+                    
+                        <section className='product' key={product.name}>
+                        <div className='productImg'>
+                            <img src={product.image} alt={product.name}/>
+                            </div>
+                            <div className='productDesc'>
+                            <p>{product.name}</p>
+                            <p>$ {product.price}</p>
+                            <button onClick={()=>addCartHandler(product)} className='btn primry'>Add to cart</button>
+                        </div>
+                        </section>
+                )
+                })}
+            </section>
+        </main>
+     );
 }
  
 export default Home;
