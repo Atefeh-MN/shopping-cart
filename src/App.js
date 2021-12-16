f87a55fd42ecfcd057db17f4b4876f257e9bcb5c
Atefeh_MN <atefeh.nasrabadi1993@gmail.com>
2021-12-16 23:00:11 +0330

    add action ADDTOCART to reducer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import {
   Routes, Route
 } from "react-router-dom";
 import './App.css';
+import CartProvider from "./context/provider/CartProvider";
 import Layout from "./layout/Layout";
 import CartPage from "./pages/CartPage";
 import Home from "./pages/HomePage";
 
 function App() {
   return (
+    <CartProvider>
     <Layout>
       <Routes>
         <Route path='/' exact element={<Home />} />
         <Route path='/cart' element={<CartPage/>}/>
       </Routes>
-    </Layout>
+      </Layout>
+    </CartProvider>
 
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
+import { useCartActions } from '../context/provider/CartProvider';
 import * as data from '../data'
 const Home = () => {
+    const dispatch = useCartActions();
     const addCartHandler = (product) => {
-        console.log(product)
+        dispatch({ type: 'ADD_TO_CART', payload: product });
     }
     return (
         <main className='container'>
             <section  className='productList'>
                 {data.products.map((product) =>{return (
                     
-                        <section className='product' key={product.name}>
+                        <section className='product' key={product.id}>
                         <div className='productImg'>
                             <img src={product.image} alt={product.name}/>
                             </div>
